14a2383701fc50c553e6bd5bfc4d4ab4e9c0a88f
Sumaiyaakter98 <sa9075971@gmail.com>
2025-12-30 19:00:00 +0600

    Fixed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://event-listing-platform-8pim.vercel.app/api",
+  baseURL: "https://event-listing-platform-server.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
